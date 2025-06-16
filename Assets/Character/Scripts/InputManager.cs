36fc28da7ed46651151d546754999a07fa730941
kylebroadvr <kyle2.broad@oaklands.ac.uk>
2025-06-16 15:57:47 +0100

    3D Update
    I got a Stack Overflow exception, so thought I'd push. Probably memory on my PC

---
+++ Assets/Character/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    InputSystem_Actions playerControls;
+
+    Vector2 movementInput;
+
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new InputSystem_Actions();
+
+            playerControls.Player.Move.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+    }
+}
---
+++ Assets/Character/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    InputManager inputManager;
+    
+    Transform cameraObject;
+    Vector3 moveDirection;
+    Rigidbody playerRB;
+
+    public float movementSpeed = 7f;
+    public float rotationSpeed = 15f;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        playerRB = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleAllMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection = moveDirection * movementSpeed;
+
+        Vector3 movementVelocity = moveDirection;
+        playerRB.linearVelocity = movementVelocity;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+}
