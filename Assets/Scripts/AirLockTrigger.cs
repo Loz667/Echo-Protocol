b076cd648f4fde156fbae2c90805bee737891749
Loren Penny <loren_667@hotmail.com>
2025-06-19 16:44:42 +0100

    Added timer to Air Lock Puzzle

--- Assets/Scripts/AirLockTrigger.cs
+++ Assets/Scripts/AirLockTrigger.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AirLockTrigger : MonoBehaviour
@@ -7,13 +6,31 @@ public class AirLockTrigger : MonoBehaviour
     [SerializeField] private Space_Door linkedDoor1;
     [SerializeField] private Space_Door linkedDoor2;
 
+    [SerializeField] private float timeToVent = 5f;
+    private float ventTimer = 0f;
+
+    private bool inRange = false;
+
+    private void Update()
+    {
+        if (inRange)
+        {
+            airLockController.VentAirLock();
+            ventTimer += Time.deltaTime;
+            if (ventTimer >= timeToVent)
+            {
+                linkedDoor1.OpenDoor();
+                linkedDoor2.OpenDoor();
+                ventTimer = 0f; // Reset timer after venting
+            }
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") || other.CompareTag("Ghost"))
         {
-            airLockController.VentAirLock();
-            linkedDoor1.OpenDoor();
-            linkedDoor2.OpenDoor();
+            inRange = true;
         }
     }
 
@@ -21,6 +38,7 @@ public class AirLockTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player") || other.CompareTag("Ghost"))
         {
+            inRange = false;
             airLockController.ResetAirlock();
             linkedDoor1.CloseDoor();
             linkedDoor2.CloseDoor();
